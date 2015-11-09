d7c352dbaa080825f43bea98535235f2d909bd4f
Yann-Aël Le Borgne <yannael@gmail.com>
2015-11-09 12:57:55 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -2,8 +2,8 @@ rm -rf BRiDGEIrisPortal
 git clone https://github.com/BRiDGEIris/BRiDGEIrisPortal
 
 . ./portal.conf
-sed -i bak -e "s|BASE_URL_GVR|$BASE_URL_GVR|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
-sed -i bak -e "s|BASE_URL_CLINIPHENOME|$BASE_URL_CLINIPHENOME|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
-sed -i bak -e "s|BASE_URL_DIDA|$BASE_URL_DIDA|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
+sed -i -e "s|BASE_URL_GVR|$BASE_URL_GVR|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
+sed -i -e "s|BASE_URL_CLINIPHENOME|$BASE_URL_CLINIPHENOME|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
+sed -i -e "s|BASE_URL_DIDA|$BASE_URL_DIDA|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
 
 docker build -t portal .
