e626f7cc1654c129d940765f74e9a0b3a2805fa8
Yann-Aël Le Borgne <yannael@gmail.com>
2015-11-09 11:14:56 +0100

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 rm -rf BRiDGEIrisPortal
 git clone https://github.com/BRiDGEIris/BRiDGEIrisPortal
-. ../env_conf.sh
+
+. ./portal.conf
 sed -i bak -e "s|BASE_URL_GVR|$BASE_URL_GVR|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
 sed -i bak -e "s|BASE_URL_CLINIPHENOME|$BASE_URL_CLINIPHENOME|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
 sed -i bak -e "s|BASE_URL_DIDA|$BASE_URL_DIDA|g" BRiDGEIrisPortal/BrideIRISportalsite/index.php
