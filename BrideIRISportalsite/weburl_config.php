c447901efd088e59223217794fef3b37c891e5ee
Dipankar Sengupta <dipankarsengupta.1982@gmail.com>
2016-02-09 10:59:14 +0100

    config

--- BrideIRISportalsite/weburl_config.php
+++ BrideIRISportalsite/weburl_config.php
@@ -2,8 +2,8 @@
 //URL configuration file for the resources in BRiDGEIris Portal
 
 $highlander = 'http://sites.uclouvain.be/highlander/download.html';
-$cliniphenome = 'http://localhost/ClinicalDBHome.php';
+$cliniphenome = 'http://bridgeiris.ulb.ac.be/cliniphenome/';
 $dida = 'http://dida.ibsquare.be/';
 $digest = 'http://bridgeiris.ulb.ac.be:82/digest/';
-$gevact = 'http://localhost/bridgeirisportal/gevact/index.php';
+$gevact = 'http://bridgeiris.ulb.ac.be:81/gevact/';
 ?>
